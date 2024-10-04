f79b23c66df86c43d6fc2615dccd7ce304e47bb9
Bengt-Erik Fröberg <bengt-erik.froeberg@redpill-linpro.com>
2024-10-04 11:15:43 +0200

    Add gateway-api

---
+++ api-gateway/src/test/java/net/benfro/poc/library/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package net.benfro.poc.library;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
