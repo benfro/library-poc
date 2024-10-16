613d2bb07adc16c80f6d4128dc8bc49c4453d556
Bengt-Erik Fröberg (bengfrob) <bengt-erik.froberg@tele2.com>
2024-10-16 11:28:13 +0200

    library-core

---
+++ library-core/src/main/java/net/benfro/library/core/config/DatabaseConfig.java
@@ -0,0 +1,39 @@
+package net.benfro.library.core.config;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig extends AbstractR2dbcConfiguration {
+
+    @Value("${database.host}") String host;
+    @Value("${database.port}") int port;
+    @Value("${database.name}") String database;
+    @Value("${database.user}") String user;
+    @Value("${database.password}") String password;
+
+    @NotNull
+    @Bean
+    @Override
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
+            .option(DRIVER, "pool")
+            .option(PROTOCOL, "postgresql")
+            .option(HOST, host)
+            .option(PORT, port)
+            .option(USER, user)
+            .option(PASSWORD, password)
+            .option(DATABASE, database)
+            .build());
+    }
+}
