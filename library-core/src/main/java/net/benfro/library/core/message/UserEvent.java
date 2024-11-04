bb425e5bd4a6723dd6fe9c6d29296548553b35af
Bengt-Erik Fröberg (bengfrob) <bengt-erik.froberg@tele2.com>
2024-11-04 11:50:45 +0100

    Upped Spring Boot version

--- library-core/src/main/java/net/benfro/library/core/message/UserEvent.java
+++ library-core/src/main/java/net/benfro/library/core/message/UserEvent.java
@@ -2,10 +2,17 @@ package net.benfro.library.core.message;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Getter
-public record UserEvent(Action action, String fullName, String email, UUID userKey) {
+@Builder
+public record UserEvent(
+        @Getter Action action,
+        @Getter String fullName,
+        @Getter String email,
+        @Getter UUID userKey) {
     public enum Action {
         USER_CREATED,
         USER_UPDATED,
